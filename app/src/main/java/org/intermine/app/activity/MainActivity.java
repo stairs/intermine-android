794cbe5413ed58db3cfa974b8007d3892f10dcca
Daria <daria_komkova@hotmail.com>
2015-09-26 17:01:07 +0300

    add mines selection option

--- app/src/main/java/org/intermine/app/activity/MainActivity.java
+++ app/src/main/java/org/intermine/app/activity/MainActivity.java
@@ -18,7 +18,9 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import org.intermine.app.R;
 import org.intermine.app.core.Gene;
@@ -32,8 +34,11 @@ import org.intermine.app.fragment.SearchFragment;
 import org.intermine.app.fragment.TemplatesFragment;
 import org.intermine.app.listener.OnGeneSelectedListener;
 
+import java.util.Set;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity implements OnGeneSelectedListener,
         ListsFragment.OnListSelectedListener, TemplatesFragment.OnTemplateSelectedListener {
@@ -43,9 +48,17 @@ public class MainActivity extends BaseActivity implements OnGeneSelectedListener
     @InjectView(R.id.drawer_layout)
     protected DrawerLayout mDrawerLayout;
 
+    @InjectView(R.id.navigation_view)
+    protected NavigationView mNavigationView;
+
+    @InjectView(R.id.mine_name)
+    protected TextView mMineNameView;
+
     private List mGeneFavoritesList;
 
     protected CharSequence mTitle;
+    private String mMineName;
+    private boolean mMainMenuDisplayed;
 
     // --------------------------------------------------------------------------------------------
     // Activity Lifecycle
@@ -57,43 +70,62 @@ public class MainActivity extends BaseActivity implements OnGeneSelectedListener
         setContentView(R.layout.main_activity);
         ButterKnife.inject(this);
 
+        populateContentFragment(SearchFragment.newInstance());
         initToolbar();
         setupDrawerLayout();
 
         mGeneFavoritesList = new List(getString(R.string.gene_favorites_list_name));
-        getStorage().getMineNameToUrlMap();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMineName = getStorage().getWorkingMineName();
+        Set<String> mines = getStorage().getSelectedMineNames();
+
+        if (!mines.contains(mMineName)) {
+            mMineName = mines.iterator().next();
+            getStorage().setWorkingMineName(mMineName);
+        }
+        mMineNameView.setText(mMineName);
     }
 
     private void setupDrawerLayout() {
-        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
-        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        mMainMenuDisplayed = true;
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-                Fragment fragment = null;
-                String mineName = "FlyMine";
-                switch (menuItem.getItemId()) {
-                    case R.id.drawer_search_all:
-                        fragment = SearchFragment.newInstance();
-                        break;
-                    case R.id.drawer_templates:
-                        fragment = TemplatesFragment.newInstance(mineName);
-                        break;
-                    case R.id.drawer_lists:
-                        fragment = ListsFragment.newInstance(mineName);
-                        break;
-                    case R.id.drawer_favourites:
-                        fragment = GenesListFragment.newInstance(mGeneFavoritesList, mineName);
-                        break;
-                    case R.id.drawer_login:
-                        fragment = LogInFragment.newInstance();
-                        break;
-                    case R.id.drawer_info:
-                        fragment = InfoFragment.newInstance();
-                        break;
+                if (mMainMenuDisplayed) {
+                    Fragment fragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.drawer_search_all:
+                            fragment = SearchFragment.newInstance();
+                            break;
+                        case R.id.drawer_templates:
+                            fragment = TemplatesFragment.newInstance(mMineName);
+                            break;
+                        case R.id.drawer_lists:
+                            fragment = ListsFragment.newInstance(mMineName);
+                            break;
+                        case R.id.drawer_favourites:
+                            fragment = GenesListFragment.newInstance(mGeneFavoritesList, mMineName);
+                            break;
+                        case R.id.drawer_login:
+                            fragment = LogInFragment.newInstance();
+                            break;
+                        case R.id.drawer_info:
+                            fragment = InfoFragment.newInstance();
+                            break;
+                    }
+                    populateContentFragment(fragment);
+                    mDrawerLayout.closeDrawers();
+                } else {
+                    mMineName = menuItem.getTitle().toString();
+                    getStorage().setWorkingMineName(mMineName);
+                    mMineNameView.setText(mMineName);
+                    onDrawerHeaderClick();
                 }
-                populateContentFragment(fragment);
-                //menuItem.setChecked(true);
-                mDrawerLayout.closeDrawers();
                 return true;
             }
         });
@@ -110,8 +142,15 @@ public class MainActivity extends BaseActivity implements OnGeneSelectedListener
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.drawer_header)
+    public void onDrawerHeaderClick() {
+        setMainMenuDisplayed(mMainMenuDisplayed);
+        toggleExpandedView(mMainMenuDisplayed);
+
+        mMainMenuDisplayed = !mMainMenuDisplayed;
     }
 
     public void onSectionAttached(String title) {
@@ -158,6 +197,36 @@ public class MainActivity extends BaseActivity implements OnGeneSelectedListener
     }
 
     protected void populateContentFragment(Fragment fragment) {
-        getFragmentManager().beginTransaction().replace(R.id.main_container, fragment).commit();
+        if (null != fragment) {
+            getFragmentManager().beginTransaction().replace(R.id.main_container, fragment).commit();
+        }
+    }
+
+    private void setMainMenuDisplayed(boolean displayed) {
+        Menu menu = mNavigationView.getMenu();
+        menu.setGroupVisible(R.id.main, !displayed);
+        menu.setGroupVisible(R.id.rest, !displayed);
+
+        if (displayed) {
+            menu.removeGroup(R.id.mines);
+            Set<String> mines = getStorage().getSelectedMineNames();
+            int counter = 0;
+
+            for (String mine : mines) {
+                menu.add(R.id.mines, counter, counter, mine);
+                counter++;
+            }
+        }
+        menu.setGroupVisible(R.id.mines, displayed);
+    }
+
+    private void toggleExpandedView(boolean expanded) {
+        if (expanded) {
+            mMineNameView.setCompoundDrawablesWithIntrinsicBounds(null, null,
+                    getResources().getDrawable(R.drawable.ic_expand_less_white_24dp), null);
+        } else {
+            mMineNameView.setCompoundDrawablesWithIntrinsicBounds(null, null,
+                    getResources().getDrawable(R.drawable.ic_expand_more_white_24dp), null);
+        }
     }
 }
